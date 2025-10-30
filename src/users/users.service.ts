1ff7723f80f45bfa7cb58406dbd4a050212b52e0
likhithkp <likhithkpdev@gmail.com>
2025-10-30 18:38:20 +0530

    "Push v2"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  ConflictException,
-  NotFoundException,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
